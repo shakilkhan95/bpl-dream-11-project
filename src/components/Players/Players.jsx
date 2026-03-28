e0504e17a0300ac87e0ee36858c59571a918eab8
shakilkhan95 <th.shakilkhan@gmail.com>
2026-03-29 03:29:16 +0400

    added state to to toggle between buttons available and selected and rendered btn bg color based on state and changed the section title

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import PlayerCard from './PlayerCard';
 
 
@@ -6,18 +6,32 @@ const Players = ({playerDataPromise}) => {
     const playersData = use(playerDataPromise);
     const players = playersData.players;
 
+    const [selectedType, setSelectedType] = useState('available');
+
   return (
     <section className="x-2.5 lg:mx-10 py-5 lg:py-10 mt-10">
       <div className="flex justify-between items-center mb-5">
-        <h3 className="text-2xl font-semibold text-[#131313]">
-          Available Player: {players.length}
-        </h3>
+        {selectedType === "available" ? (
+          <h3 className="text-2xl font-semibold text-[#131313]">
+            Available Player: {players.length}
+          </h3>
+        ) : (
+          <h3 className="text-2xl font-semibold text-[#131313]">
+            Selected Player: {players.length}
+          </h3>
+        )}
         <div className="space-x-5">
-          <button className="px-6 py-3 bg-[#E7FE29] rounded-lg font-semibold cursor-pointer">
+          <button
+            onClick={() => setSelectedType("available")}
+            className={`px-6 py-3 ${selectedType === "available" ? "bg-[#E7FE29]" : "bg-base-300"} rounded-lg font-semibold cursor-pointer`}
+          >
             Available
           </button>
-          <button className="px-6 py-3 bg-base-300 rounded-lg font-semibold cursor-pointer">
-            Selected
+          <button
+            onClick={() => setSelectedType("selected")}
+            className={`px-6 py-3 ${selectedType === "selected" ? "bg-[#E7FE29]" : "bg-base-300"} rounded-lg font-semibold cursor-pointer`}
+          >
+            Selected (0)
           </button>
         </div>
       </div>
